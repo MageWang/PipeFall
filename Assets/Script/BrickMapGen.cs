71be3f08be537e06c96b75775a2327901fe6d95d
mage.wang <wangm4a1@hotmail.com>
2018-11-27 11:52:18 +0800

    add brickMapGen.

---
+++ Assets/Script/BrickMapGen.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickMapGen : MonoBehaviour {
+	static private BrickMapGen instance;
+	static public BrickMapGen GetInstance(){
+		return instance;
+	}
+	public List<List<Brick.Direction[]>> directions = new List<List<Brick.Direction[]>>();
+	// Use this for initialization
+	public int width = 6;
+	void Awake(){
+		if(instance!=null){
+			Debug.LogWarning("instance!=null");
+			return;
+		}
+		instance = this;
+	}
+	
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public Brick.Direction[] At(int x, int y){
+		if(y > 2000){
+			
+		}
+		if(directions.Count < y-1){
+			// gen new
+		}
+		if(x > width -1){
+			Debug.LogWarning("x > width -1");
+			return new Brick.Direction[2]{Brick.Direction.none, Brick.Direction.none};
+		}
+		return directions[y][x];
+	}
+}

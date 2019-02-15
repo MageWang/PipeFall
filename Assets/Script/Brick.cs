8e1161f370165e082ba9860ac799feca0f07a3fc
Mage <mage@abcdefg.hijklmnop>
2019-02-15 16:48:47 +0800

    replace pipe shader.

--- Assets/Script/Brick.cs
+++ Assets/Script/Brick.cs
@@ -125,7 +125,7 @@ public class Brick : MonoBehaviour, IDragHandler, IEndDragHandler {
 
 	void ResetSprite(){
 		pipeObject.SetActive(true);
-		waterMaterial.SetFloat("_Opacity", Mathf.Lerp(1.0f, 0.5f,progress));
+		waterMaterial.SetFloat("_Opacity", Mathf.Lerp(1.0f, 0.0f,progress));
 
 		bool isLeft, isRight, isTop, isBot;
 		isLeft = isRight = isTop = isBot = false;
